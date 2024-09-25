b6d9fc7b6f71dd2f25410224c84a2818187f130e
Ic-arbon <dty2015@hotmail.com>
2024-09-25 13:25:59 +0800

    feat(hosts): enable daed for NixOS

--- hosts/tydsG16/configuration.nix
+++ hosts/tydsG16/configuration.nix
@@ -37,43 +37,43 @@
   # services.v2ray.configFile = "/etc/v2ray/config.json";
 
   # dae
-  services.dae = {
+  # services.dae = {
+  #     enable = true;
+  #
+  #     openFirewall = {
+  #       enable = true;
+  #       port = 12345;
+  #     };
+  #
+  #     configFile = "/home/tyd/config.dae";
+  #
+  #     /* default options */
+  #
+  #     package = inputs.daeuniverse.packages.x86_64-linux.dae;
+  #     # disableTxChecksumIpGeneric = false;
+  #     #
+  #     # configFile = "/etc/dae/config.dae";
+  #     # assets = with pkgs; [ v2ray-geoip v2ray-domain-list-community ];
+  #
+  #     # alternative of `assets`, a dir contains geo database.
+  #     # assetsPath = "/etc/dae";
+  # };
+  # daed - dae with a web dashboard
+  services.daed = {
       enable = true;
 
-      # openFirewall = {
-      #   enable = true;
-      #   port = 12345;
-      # };
+      openFirewall = {
+        enable = true;
+        port = 12345;
+      };
 
-      configFile = "/home/tyd/config.dae";
-
-      /* default options */
-
-      package = inputs.daeuniverse.packages.x86_64-linux.dae;
-      # disableTxChecksumIpGeneric = false;
-      #
-      # configFile = "/etc/dae/config.dae";
-      # assets = with pkgs; [ v2ray-geoip v2ray-domain-list-community ];
-
-      # alternative of `assets`, a dir contains geo database.
-      # assetsPath = "/etc/dae";
-  };
-  # daed - dae with a web dashboard
-  # services.daed = {
-  #     enable = true;
-
-      # openFirewall = {
-      #   enable = true;
-      #   port = 12345;
-      # };
-
-      # listen = "0.0.0.0:2023";
+      listen = "0.0.0.0:2023";
       /* default options */
 
       # package = inputs.daeuniverse.packages.x86_64-linux.daed;
       # configDir = "/etc/daed";
       # listen = "127.0.0.1:2023";
-  # };
+  };
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
