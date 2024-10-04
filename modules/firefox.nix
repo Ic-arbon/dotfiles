023a92d4b0ae7e92c97bfb6c9e72375f123c0777
Ic-arbon <dty2015@hotmail.com>
2024-10-04 16:36:34 +0800

    feat(modules): auto set firefox default profile

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -9,7 +9,35 @@
 
   programs.firefox = {
     enable = true;
-    package = config.lib.nixGL.wrap pkgs.firefox-devedition-bin;
-    # languagePacks = [ "en-US" "zh-CN"];
+    # package = config.lib.nixGL.wrap pkgs.firefox-devedition-bin;
+    package = config.lib.nixGL.wrap pkgs.firefox;
+    languagePacks = [ "en-US" "zh-CN"];
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      DontCheckDefaultBrowser = true;
+      DisablePocket = true;
+      SearchBar = "unified";
+    };
+    profiles = {
+
+      dev-edition-default = {
+        id = 0;
+        name = "profile_0";
+        isDefault = true;
+        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+          adblocker-ultimate
+          darkreader
+          tampermonkey
+          translate-web-pages
+          gruvbox-dark-theme
+          kristofferhagen-nord-theme
+        ];
+        settings = {
+          "extensions.autoDisableScopes" = 0;
+        };
+      };
+
+    };
   };
 }
