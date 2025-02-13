b0ba413f54887feb68e2a5c93208ef2920aa7199
Ic-arbon <dty2015@hotmail.com>
2025-02-13 21:44:38 +0800

    fix(modules): err in scripts to enable ipv6 & err in gtk extra config syntax

--- modules/hyprland/theme.nix
+++ modules/hyprland/theme.nix
@@ -12,37 +12,6 @@
       platformTheme.name = "gtk";
   };
 
-  # gtk = {
-  #   enable = true;
-  #
-  #   theme = {
-  #     package = pkgs.flat-remix-gtk;
-  #     name = "Flat-Remix-GTK-Grey-Darkest";
-  #   };
-  #
-  #   iconTheme = {
-  #     # package = pkgs.gnome.adwaita-icon-theme;
-  #     package = pkgs.adwaita-icon-theme;
-  #     name = "Adwaita";
-  #   };
-  #
-  #   gtk3.extraConfig = {
-  #     Settings = ''
-  #       gtk-application-prefer-dark-theme=1
-  #     '';
-  #   };
-  #
-  #   gtk4.extraConfig = {
-  #     Settings = ''
-  #       gtk-application-prefer-dark-theme=1
-  #     '';
-  #   };
-  #
-  #   font = {
-  #     name = "Sans";
-  #     size = 11;
-  #   };
-  # };
   gtk = {
     enable = true;
     iconTheme = {
@@ -61,14 +30,12 @@
         };
     };
     gtk3.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
+        gtk-application-prefer-dark-theme=1;
+        gtk-im-module="fcitx";
     };
     gtk4.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
+        gtk-application-prefer-dark-theme=1;
+        gtk-im-module="fcitx";
     };
   };
 }
