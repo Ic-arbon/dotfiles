0615f3d6e7138c66a3715b984d34684ede574f12
Ic-arbon <dty2015@hotmail.com>
2025-07-05 14:11:40 +0800

    feat(input): 增强 fcitx5 输入法配置
    - 启用 Wayland 前端支持
    - 添加输入法组配置，默认使用拼音输入
    - 启用云拼音功能提升输入体验
    
    完善了 Wayland 环境下的中文输入体验

--- modules/fcitx5.nix
+++ modules/fcitx5.nix
@@ -3,6 +3,7 @@
     # enabled = "fcitx5";
     enable = true;
     type = "fcitx5";
+    fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [
       fcitx5-chinese-addons
       fcitx5-pinyin-zhwiki
@@ -11,5 +12,19 @@
       fcitx5-gtk
       fcitx5-configtool
     ];
+    fcitx5.settings.inputMethod = {
+      GroupOrder."0" = "Default";
+      "Groups/0" = {
+        Name = "Default";
+        "Default Layout" = "us";
+        DefaultIM = "pinyin";
+      };
+      "Groups/0/Items/0".Name = "keyboard-us";
+      "Groups/0/Items/1".Name = "pinyin";
+    };
+    fcitx5.settings.addons.pinyin.globalSection = {
+      CloudPinyinEnabled = "True";
+      CloudPinyinIndex = 2;
+    };
   };
 }
