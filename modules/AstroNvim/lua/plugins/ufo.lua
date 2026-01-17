c4f0b7dbf9b66cb82433ef0912557273e74e1115
Ic-arbon <dty2015@hotmail.com>
2026-01-17 17:22:17 +0800

    feat(astronvim): 添加新插件 pantran、ufo 和 nvim-dap-probe-rs
    - pantran: Google 翻译集成，默认中文目标语言
    - ufo: 代码折叠增强 (锁定特定版本)
    - nvim-dap-probe-rs: 嵌入式 Rust 调试支持 (RTT 日志)

---
+++ modules/AstroNvim/lua/plugins/ufo.lua
@@ -0,0 +1,9 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+  {
+    "kevinhwang91/nvim-ufo",
+    commit = "5b75cf5fdb74054fc8badb2e7ca9911dc0470d94",
+    dependencies = { "kevinhwang91/promise-async" },
+  },
+}
